bf549fbe8a2c341bd50f1bd7fdc6642e4e4abd03
davidoffdado <davidruffini98@gmail.com>
2024-01-16 00:28:18 +0100

    w

--- javascript/script.js
+++ javascript/script.js
@@ -1,39 +1,59 @@
 $(document).ready(function(){
     // MUST REFACTOR THIS
 
-    $('#work').on('click', function(){
+    $('#italia').on('click', function(){
         $('.nav-item').css('color', '#adadad');
-        $('#fun-section').css('display','none');
+        $('#medio-oriente-section').css('display','none');
+	$('#ucraina-section').css('display','none');
         $('#about-section').css('display','none');
-        $('#work-section').css('display','block').css('transition','0.2s');
+        $('#italia-section').css('display','block').css('transition','0.2s');
 
-        $('#work').addClass('selected-nav');
-        $('#fun').removeClass('selected-nav');
+        $('#italia').addClass('selected-nav');
+        $('#medio-oriente').removeClass('selected-nav');
+	$('#ucraina').removeClass('selected-nav');
         $('#about').removeClass('selected-nav');
         $('.nav-item', this).css('color', '#000');
     });
 
-    $('#fun').on('click',function() {
+    $('#medio-oriente').on('click',function() {
         $('.nav-item').css('color', '#adadad');
-        $('#work-section').css('display','none');
+        $('#italia-section').css('display','none');
+	$('#ucraina-section').css('display','none');
         $('#about-section').css('display','none');
-        $('#fun-section').css('display','block').css('transition','0.2s');
+        $('#medio-oriente-section').css('display','block').css('transition','0.2s');
 
-        $('#fun').addClass('selected-nav');
-        $('#work').removeClass('selected-nav');
+        $('#medio-oriente').addClass('selected-nav');
+        $('#italia').removeClass('selected-nav');
+	$('#ucraina').removeClass('selected-nav');
         $('#about').removeClass('selected-nav');
+        $('.nav-item', this).css('color', '#000');
+    });
+
+    $('#ucraina').on('click', function(){
+        $('.nav-item').css('color', '#adadad');
+        $('#medio-oriente-section').css('display','none');
+        $('#italia-section').css('display','none');
+	$('#about-section').css('display','none');
+        $('#ucraina-section').css('display','block').css('transition','0.2s');
+
+        $('#ucraina').addClass('selected-nav');
+        $('#italia').removeClass('selected-nav');
+	$('#about').removeClass('selected-nav');
+        $('#medio-oriente').removeClass('selected-nav');
         $('.nav-item', this).css('color', '#000');
     });
 
     $('#about').on('click', function(){
         $('.nav-item').css('color', '#adadad');
-        $('#fun-section').css('display','none');
-        $('#work-section').css('display','none');
+        $('#medio-oriente-section').css('display','none');
+	$('#ucraina-section').css('display','none');
+        $('#italia-section').css('display','none');
         $('#about-section').css('display','block').css('transition','0.2s');
 
         $('#about').addClass('selected-nav');
-        $('#work').removeClass('selected-nav');
-        $('#fun').removeClass('selected-nav');
+        $('#italia').removeClass('selected-nav');
+        $('#medio-oriente').removeClass('selected-nav');
+	$('#ucraina').removeClass('selected-nav');
         $('.nav-item', this).css('color', '#000');
     });
 
